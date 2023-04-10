9abe353252db22c108d2c689e35e30e3d9ce6c6f
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2023-04-10 08:00:09 +0530

    Update 2399-check-distances-between-same-letters.cpp

--- 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.cpp
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.cpp
@@ -1,3 +1,5 @@
+//                    O(N) Time and O(N) Space
+
 class Solution {
 public:
     bool checkDistances(string s, vector<int>& distance) {
@@ -9,6 +11,21 @@ public:
             }
         }
         return true;  
-        
+    }
+};
+
+//             O(N*N) Time and O(1) Space
+class Solution {
+public:
+    bool checkDistances(string s, vector<int>& distance) {
+        unordered_map<char, int> mp;
+        for(int i=0; i<s.length(); i++){
+            for(int j=i+1; j<s.length(); j++){
+                if(s[i] == s[j]){
+                    if(j-i-1 != distance[s[i]-'a']) return false;
+                }
+            }
+        }
+        return true;    
     }
 };
