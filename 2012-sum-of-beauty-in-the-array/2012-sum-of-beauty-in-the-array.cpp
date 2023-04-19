44e3ee9b8d0ccc95a884889a113e781c681c878b
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2023-04-19 11:56:11 +0530

    Time: 172 ms (57.42%), Space: 85.1 MB (82.20%) - LeetHub

---
+++ 2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int sumOfBeauties(vector<int>& nums) {
+        //brute force : check for every index T.C = O(n*n), give TLE
+        //use prefix max(no need to maintain array) and postfix min arrays
+        //T.C = O(n) && S.C = O(n)
+        int n = nums.size();
+        int mx = nums[0];
+        //find postfix min array
+        vector<int> postmin(n);
+        postmin[n-1] = nums[n-1];
+        for(int i=n-2; i >= 0; i--) postmin[i] = min(postmin[i+1], nums[i]);
+        
+        int ans = 0;
+        //now traverse
+        for(int i=1; i<n-1; i++){
+            if(mx < nums[i] && nums[i] < postmin[i+1]) ans += 2;
+            else if(nums[i-1] < nums[i] && nums[i] < nums[i+1]) ans += 1;
+            //update mx(because we are not using prefix max array)
+            mx = max(mx, nums[i]);
+        }
+        return ans;
+    }
+};
