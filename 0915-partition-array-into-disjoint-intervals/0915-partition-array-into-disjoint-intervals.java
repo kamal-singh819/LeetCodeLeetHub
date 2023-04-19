a89a3fa1cc80acfc876d9353b3ee4cb567828e77
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2023-04-19 11:38:06 +0530

    Time: 4 ms (56.35%), Space: 57.7 MB (16.61%) - LeetHub

--- 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.java
+++ 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.java
@@ -1,11 +1,7 @@
 class Solution {
     public int partitionDisjoint(int[] nums) {
         int n = nums.length;
-        //create prefix max array (from starting)
-        int[] premax = new int[n];
-        premax[0] = nums[0];
-        for(int i=1; i<n; i++) premax[i] = Math.max(premax[i-1], nums[i]);
-        
+        int mx = -1; //no need to maintain prefixMax array---
         //create postfix min array (from last)
         int[] postmin = new int[n];
         postmin[n-1] = nums[n-1];
@@ -14,7 +10,8 @@ class Solution {
         //now find an index such that premax[i] <= postmin[i+1]
         int ans = 0;
         for(int i=0; i<n-1; i++){
-            if(premax[i] <= postmin[i+1]){
+            mx = Math.max(mx, nums[i]);
+            if(mx <= postmin[i+1]){
                 ans = i;
                 break;
             }
